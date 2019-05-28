a287bb1506f2c16cc15066f9ec99820404556505
tntwiki <tntwiki@foxmail.com>
2019-05-28 22:21:48 +0800

    修复epub章节名称含有保留符号时目录无法正常加载以及一些其他的问题。

--- program/shell/dmzj-epub.sh
+++ program/shell/dmzj-epub.sh
@@ -4,14 +4,14 @@
 #cycle* 循环跳出
 
 book1=$@
-if [ -z ${book1} ]
+if [ -z ${book1:0:1} ]
 then
     read -p "请输入书籍变量:" book1
 fi
 #函数
 function manifest_fun()
 {   #epub文件列表追加
-    manifest=$(echo ${manifest}'<item id="'${1}'" href="'${2}'" media-type="'${3}'" />')
+    manifest=$(echo ${manifest}'<item id="'${1}'" href="'${2}'" media-type="'${3}'" '${4}' />')
 }
 function spine_fun()
 {   #epub主体文件读取顺序 1.url
@@ -27,6 +27,10 @@ function 随机()
 {
     #local a=$(echo $RANDOM % 20|bc);echo "暂停${a}s" >&2;echo $a
     echo 0
+}
+function html_entity()      #对html预留符号替换为实体字符
+{
+    sed  's/&/\&amp;/g'|sed 's/"/\&quot;/g'|sed 's/</\&lt;/g'|sed 's/>/\&gt;/g'|sed "s/'/\&apos;/g"
 }
 
 
@@ -52,15 +56,15 @@ do
     mkdir -p epub/book
     mkdir -p epub/book/image
     wget -t 0 ${cover} -O epub/book/image/cover.jpg
-    manifest_fun ncx toc.ncx application/x-dtbncx+xml   #追加索引文件
+    manifest_fun ncx toc.ncx application/x-dtbncx+xml 'properties="nav" '  #追加索引文件
     manifest_fun cover_image image/cover.jpg image/jpeg #追加封面图片文件
 
     
     echo '<?xml version="1.0" encoding="UTF-8"?><package version="3.0" xmlns="http://www.idpf.org/2007/opf" unique-identifier="epub"><metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf"><dc:identifier id="epub">'"urn:uuid:$(uuidgen||cat /proc/sys/kernel/random/uuid)"'</dc:identifier><dc:title>'"${bookname}"'</dc:title><dc:language>'"${LANG%.*}"'</dc:language><dc:subject>'"${types}"'</dc:subject><dc:description>'"${introduction}"'</dc:description><dc:creator>'"${authors}"'</dc:creator><meta name="cover" content="cover_image"/></metadata>' > epub/book/index.opf #初始化包装文件
     echo '<?xml version="1.0" encoding="utf-8"?><ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1"><head></head><docTitle><text>'"${bookname}"'</text></docTitle><navMap>' > epub/book/toc.ncx  #初始化目录索引文件
-    echo '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE html><html><head>><meta charset="utf-8" /><title>'"${bookname}"'</title></head><body><p id="title_t"><img src="image/cover.jpg" /></p><h1>'${bookname}'</h1></body></html>' > epub/book/title.html   #生成标题文件
+    echo '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE html> <html xmlns="http://www.w3.org/1999/xhtml"><head><meta charset="utf-8" /><title>'"${bookname}"'</title></head><body><p id="title_t"><img src="image/cover.jpg" /></p><h1>'${bookname}'</h1></body></html>' > epub/book/title.html   #生成标题文件
     manifest_fun title_cover title.html application/xhtml+xml  #追加标题文件
-    navpoint_fun title_title ${bookname} title.html#title_t y  #目录生成(根）
+    navpoint_fun title_title "${bookname}" title.html#title_t y  #目录生成(根）
     spine_fun title.html  #载入顺序 标题1
 
 	
@@ -69,14 +73,14 @@ do
     do
 	count2=$(echo "${count2}+1"|bc)
 	volume=$(echo ${json1}|jq .id) #获取卷id
-	volume_name=$(echo ${json1}|jq .volume_name -r) #获取卷名
+	volume_name=$(echo ${json1}|jq .volume_name -r|html_entity) #获取卷名
 	json2=$(echo ${json1}|jq .chapters -r)#获取卷索引
 	count3=0
 	json3=$(echo ${json2}|jq .[${count3}])
 
 
 	#epub,html生成
-	echo '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><meta charset="utf-8" /><title>'"${bookname}-${volume_name}"'</title></head><body><h1 id='"${volume}"'>'"${volume_name}"'</h1>'  > epub/book/${volume}.html  #生成html头
+	echo '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops"><head><meta charset="utf-8" /><title>'"${bookname}-${volume_name}"'</title></head><body><h1 id="'"${volume}"'">'"${volume_name}"'</h1>'  > epub/book/${volume}.html  #生成html头
 	navpoint_fun volume_${volume} "${volume_name}" ${volume}.html  y #追加链接至目录文件 （2）
 	manifest_fun ${volume} ${volume}.html application/xhtml+xml     #追加当前卷html入包装文件
 	spine_fun ${volume}.html  #载入文件顺序列表
@@ -86,8 +90,8 @@ do
 	do
 	    count3=$(echo "${count3}+1"|bc)
 	    chapter=$(echo ${json3}|jq .chapter_id)  #获取章节id
-	    chapter_name=$(echo ${json3}|jq .chapter_name -r)  #获取章节名
-	    wget -t 0  https://v3api.dmzj.com/novel/download/${title}_${volume}_${chapter}.txt -O ${title}_${volume}_${chapter}.txt #下载正文
+	    chapter_name=$(echo ${json3}|jq .chapter_name -r|html_entity)  #获取章节名
+#	    wget -t 0  https://v3api.dmzj.com/novel/download/${title}_${volume}_${chapter}.txt -O ${title}_${volume}_${chapter}.txt #下载正文
 	    json3=$(echo ${json2}|jq .[${count3}])
 	    
 	    #epub，html
@@ -103,7 +107,7 @@ do
 	    while [ -n "${image_url}" ]
 	    do
 		rows=$(echo "${rows}+1"|bc)
-		wget -t 0  ${image_url}  -O epub/book/image/${volume}_${chapter}_${rows}.${image_url##*.}
+#		wget -t 0  ${image_url}  -O epub/book/image/${volume}_${chapter}_${rows}.${image_url##*.}
 		text_chapter=$(echo "${text_chapter}"|sed "s%${image_url}%image/${volume}_${chapter}_${rows}.${image_url##*.}%g") 
 		manifest_fun ${volume}_${chapter}_${rows} image/${volume}_${chapter}_${rows}.${image_url##*.}  image/jpeg  #列出文件
 		image_url=$(echo "${image_download}"|sed -n "${rows}p")
@@ -114,7 +118,7 @@ do
 
 	    
 	    #|tr '\n' ' '|sed 's#<br */>\r* *<br */>#\n#g'|sed 's#^#<p>#g'|sed 's#$#</p>#g'|sed 's#^<p> *</p>$# #g'|sed 's#<br */># #g'|sed "1d" >>epub/book/${volume}.html
-	    echo "${text_chapter}"|tr '\n' ' '|sed 's#<br */>\r* *<br */>#\n#g'|sed 's#</br */>#&\n#g'|sed 's#<img#\n& "#g'|sed 's#^#<p>#g'|sed 's#$#</p>#g' >> epub/book/${volume}.html
+	    echo "${text_chapter}"|tr '\n' ' '|sed 's#<br */>\r* *<br */>#\n#g'|sed 's#</br */>#&\n#g'|sed 's#<img#\n& #g'|sed 's#^#<p>#g'|sed 's#$#</p>#g' >> epub/book/${volume}.html
 	    #sed 's#<br */>#\n#g'|sed '/^\r*$/d'|sed "s#^#<p>#g"|sed "s%$%</p>%g"|sed "1d" >>${text_chapter}
 	    sleep $(随机)
 	    image_download=$(echo -n)  #重置变量
@@ -127,11 +131,11 @@ do
     #epub 生成xml结束
     echo '</navPoint></navMap></ncx>' >> epub/book/toc.ncx #结束书目录
     echo '<manifest>'"${manifest}"'</manifest>' >> epub/book/index.opf #文件列表索引结束
-    echo '<spine>'"${spine}"'</spine>' >> epub/book/index.opf #加载顺序列表索引结束
+    echo '<spine toc="ncx">'"${spine}"'</spine>' >> epub/book/index.opf #加载顺序列表索引结束
     echo '<guide></guide></package>' >> epub/book/index.opf  #结束包装文件
-    cd epub&&zip -9 -r ../../${bookname}.epub book  META-INF  minetype  #打包epub
+    cd epub&&zip -9 -r ../../${bookname}.epub minetype book  META-INF  #打包epub
+    echo 脚本确认已经修改
 
-
-    cd .. #返回脚本执行时路径
+    cd ../.. #返回脚本执行时路径
 
 done #各书之间
