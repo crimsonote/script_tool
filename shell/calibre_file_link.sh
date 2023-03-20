cd8fa4fe004aeacd070c06875c0111d0fa247d24
crimsonote <crimsonote@outlook.com>
2023-03-20 11:09:03 +0800

    使这个脚本可以在已经运行calibre客户端的情况下工作，需要fakeroot命令。

--- shell/calibre_file_link.sh
+++ shell/calibre_file_link.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # 定义calibre数据库的路径
-calibre_db_path="/home/user/文档/.calibre/"
+calibre_db_path="$1"
 
 # 定义软连接存放的根目录
-link_root="/tmp/calibre"
+link_root="$2"
 
 # 使用calibredb命令获取图书信息并转化为JSON格式
-books=$(calibredb list -f authors,id,title,formats --for-machine --with-library="${calibre_db_path}")
+# 由于calibredb不允许在已经运行了calibre的情况下执行，所以这里使用fackroot工具使其忽略这一点。
+books=$(fakeroot calibredb list -f authors,id,title,formats --for-machine --with-library="${calibre_db_path}")
 
 # 遍历每个图书
 IFS=$'\n\n'
